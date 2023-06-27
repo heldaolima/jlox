18ccd7216c686cf0e0d644ef0d83112c74e6a186
Hélder Lima <orfeowkr@gmail.com>
2023-06-27 16:26:10 -0300

    feat: statements and state

--- src/main/java/com/craftinginterpreters/jlox/Expr.java
+++ src/main/java/com/craftinginterpreters/jlox/Expr.java
@@ -4,21 +4,33 @@ import java.util.List;
 
 abstract class Expr {
   interface Visitor<R> {
+    R visitAssignExpr(Assign expr);
     R visitBinaryExpr(Binary expr);
-
     R visitGroupingExpr(Grouping expr);
-
     R visitLiteralExpr(Literal expr);
-
     R visitUnaryExpr(Unary expr);
+    R visitVariableExpr(Variable expr);
   }
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
+}
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
+
+    final Token name;
+    final Expr value;
+}
   static class Binary extends Expr {
     Binary(Expr left, Token operator, Expr right) {
       this.left = left;
       this.operator = operator;
       this.right = right;
-    }
+}
 
     @Override
     <R> R accept(Visitor<R> visitor) {
@@ -28,12 +40,11 @@ abstract class Expr {
     final Expr left;
     final Token operator;
     final Expr right;
-  }
-
+}
   static class Grouping extends Expr {
     Grouping(Expr expression) {
       this.expression = expression;
-    }
+}
 
     @Override
     <R> R accept(Visitor<R> visitor) {
@@ -41,12 +52,11 @@ abstract class Expr {
     }
 
     final Expr expression;
-  }
-
+}
   static class Literal extends Expr {
     Literal(Object value) {
       this.value = value;
-    }
+}
 
     @Override
     <R> R accept(Visitor<R> visitor) {
@@ -54,13 +64,12 @@ abstract class Expr {
     }
 
     final Object value;
-  }
-
+}
   static class Unary extends Expr {
     Unary(Token operator, Expr right) {
       this.operator = operator;
       this.right = right;
-    }
+}
 
     @Override
     <R> R accept(Visitor<R> visitor) {
@@ -69,7 +78,19 @@ abstract class Expr {
 
     final Token operator;
     final Expr right;
-  }
+}
+  static class Variable extends Expr {
+    Variable(Token name) {
+      this.name = name;
+}
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+
+    final Token name;
+}
 
   abstract <R> R accept(Visitor<R> visitor);
 }
--- src/main/java/com/craftinginterpreters/jlox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/jlox/Interpreter.java
@@ -1,15 +1,21 @@
 package com.craftinginterpreters.jlox;
 
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
 
-  void interpret(Expr expression) {
+public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+  private Environment environment = new Environment();
+
+  void interpret(List<Stmt> statements) {
     try {
-      Object value = evaluate(expression);
-      System.out.println(stringify(value));
+      for (Stmt statement: statements) {
+        execute(statement);
+      }
     } catch(RuntimeError error) {
       Lox.runtimeError(error);
     }
   }
+
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
@@ -24,10 +30,16 @@ public class Interpreter implements Expr.Visitor<Object> {
         return !isTruthy(right);
       case MINUS:
         return - (double) right;
+      default: break;
     }
 
     // unreachable
     return null;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
   }
 
   private boolean isTruthy(Object object) {
@@ -64,6 +76,60 @@ public class Interpreter implements Expr.Visitor<Object> {
 
   private Object evaluate(Expr expr) {
     return expr.accept(this);
+  }
+
+  private void execute(Stmt stmt) {
+    stmt.accept(this);
+  }
+
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
+  void executeBlock(List<Stmt> statements, Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
+      
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
+    }
+  }
+
+  @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt) {
+    evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt) {
+    Object value = evaluate(stmt.expression);
+    System.out.println(stringify(value));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, value);
+    return value;
   }
 
   @Override
@@ -107,6 +173,7 @@ public class Interpreter implements Expr.Visitor<Object> {
       case STAR:
         checkNumberOperands(expr.operator, left, right);
         return (double)left * (double)right;
+      default: break;
     }
 
     // unreachable
